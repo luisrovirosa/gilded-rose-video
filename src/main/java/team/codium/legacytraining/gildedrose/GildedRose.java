520eb9b4e77a0c682569326edbb28200df066f48
Luis Rovirosa <luisrovirosa@gmail.com>
2019-07-02 18:25:22 +0200

    refactor: remove unnecessary code

--- src/main/java/team/codium/legacytraining/gildedrose/GildedRose.java
+++ src/main/java/team/codium/legacytraining/gildedrose/GildedRose.java
@@ -1,81 +1,79 @@
 package team.codium.legacytraining.gildedrose;
 
 public class GildedRose {
-	Item[] items;
+    Item[] items;
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
-	public void updateQuality() {
-		for (Item item : items) {
-			update(item);
-		}
-	}
+    public void updateQuality() {
+        for (Item item : items) {
+            update(item);
+        }
+    }
 
-	private void update(Item item) {
-		if (isAgedBrie(item)) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
-			}
-		} else if (isBackstage(item)) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
+    private void update(Item item) {
+        if (isAgedBrie(item)) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        } else if (isBackstage(item)) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-				if (isBackstage(item)) {
-					if (item.sellIn < 11) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
-					}
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
 
-					if (item.sellIn < 6) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
-					}
-				}
-			}
-		} else {
-			if (item.quality > 0) {
-				if (!isSulfuras(item)) {
-					item.quality = item.quality - 1;
-				}
-			}
-		}
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        } else {
+            if (item.quality > 0) {
+                if (!isSulfuras(item)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        }
 
-		if (!isSulfuras(item)) {
-			item.sellIn = item.sellIn - 1;
-		}
+        if (!isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
 
-		if (item.sellIn < 0) {
-			if (!isAgedBrie(item)) {
-				if (!isBackstage(item)) {
-					if (item.quality > 0) {
-						if (!isSulfuras(item)) {
-							item.quality = item.quality - 1;
-						}
-					}
-				} else {
-					item.quality = 0;
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-			}
-		}
-	}
+        if (item.sellIn < 0) {
+            if (!isAgedBrie(item)) {
+                if (!isBackstage(item)) {
+                    if (item.quality > 0) {
+                        if (!isSulfuras(item)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
 
-	private boolean isSulfuras(Item item) {
-		return item.name.equals("Sulfuras, Hand of Ragnaros");
-	}
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
 
-	private boolean isBackstage(Item item) {
-		return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-	}
+    private boolean isBackstage(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
 
-	private boolean isAgedBrie(Item item) {
-		return item.name.equals("Aged Brie");
-	}
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
 }
