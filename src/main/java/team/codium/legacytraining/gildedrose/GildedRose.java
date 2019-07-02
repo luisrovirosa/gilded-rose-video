013ac285047d035918c309a27761f9ec0533f498
Luis Rovirosa <luisrovirosa@gmail.com>
2019-07-02 18:41:15 +0200

    refactor: move logic

--- src/main/java/team/codium/legacytraining/gildedrose/GildedRose.java
+++ src/main/java/team/codium/legacytraining/gildedrose/GildedRose.java
@@ -16,6 +16,9 @@ public class GildedRose {
     private void update(Item item) {
         if (isAgedBrie(item)) {
             increaseQuality(item);
+            if (item.sellIn < 1) {
+                increaseQuality(item);
+            }
         } else if (isBackstage(item)) {
             increaseQuality(item);
 
@@ -26,26 +29,23 @@ public class GildedRose {
             if (item.sellIn < 6) {
                 increaseQuality(item);
             }
-        } else if (isSulfuras(item)) {
-
-        } else {
-            decreaseQuality(item);
-        }
-
-        if (isAgedBrie(item)) {
-            if (item.sellIn < 1) {
-                increaseQuality(item);
-            }
-        } else if (isBackstage(item)) {
             if (item.sellIn < 1) {
                 dropQualityToZero(item);
             }
         } else if (isSulfuras(item)) {
 
         } else {
+            decreaseQuality(item);
             if (item.sellIn < 1) {
                 decreaseQuality(item);
             }
+        }
+
+        if (isAgedBrie(item)) {
+        } else if (isBackstage(item)) {
+        } else if (isSulfuras(item)) {
+
+        } else {
         }
 
         if (!isSulfuras(item)) {
